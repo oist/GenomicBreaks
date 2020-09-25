75f38b278f545f2c4fe2e99fcaf43673e55aaea7
Charles Plessy <charles.plessy@oist.jp>
2020-09-25 02:30:03 +0000

    Typos

--- R/tan_bp.R
+++ R/tan_bp.R
@@ -1,9 +1,9 @@
 #' Breakpoint proximity to Tandem Repeats
 #'
-#' This function classifies breakpoints in terms of its proximity to tandem repeats (but can be generalised to any property whos coverage is binary).
+#' This function classifies breakpoints in terms of its proximity to tandem repeats (but can be generalised to any property whose coverage is binary).
 #'
 #' @param gr_ob GRanges object containing pairwise alignment
-#' @param tan GRanges object containing tandem repeat (or characterisitc of interest) coverage
+#' @param tan GRanges object containing tandem repeat (or characteristic of interest) coverage
 #' @param tol range in which breakpoints will be classified as "near" to a tandem repeat
 #' @param query_tf logical value that, if set to TRUE, will classify bps in query coordinates as opposed to reference (default)
 #' @return A list containing 3 GRanges objects of breakpoints far, near and within tandem repeats.
