a9d39000f199eafb18c1b2c8fa2f3467464a85da
Charles Plessy <charles.plessy@oist.jp>
2020-10-12 07:35:19 +0000

    Pass a genome name rather than its seqinfo.
    This avoids an annoying warning message that says:
    In is.na(genome) : is.na() applied to non-(list or vector) of type 'S4'

--- R/load_genomic_breaks.R
+++ R/load_genomic_breaks.R
@@ -28,7 +28,7 @@
 #' @export
 
 load_genomic_breaks <- function(file, bsgenome_ref, bsgenome_query) {
-  gb <- import.gff3(file, genome = seqinfo(bsgenome_ref))
+  gb <- import.gff3(file, genome = deparse(substitute(bsgenome_ref)))
   # Discard cross_genome_match parent (used for block display in Zenbu)
   gb <- gb[gb$type == "match_part"]
   # Discard unused information
