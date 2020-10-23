15ddcd29eec2edb724b46ca9dc00b772b73a55a7
Charles Plessy <charles.plessy@oist.jp>
2020-10-23 01:54:05 +0000

    Detect genome name automagically.

--- R/bp_heatmap.R
+++ R/bp_heatmap.R
@@ -3,7 +3,7 @@
 #' This function generates a Heatmap of the specified pattern, over breakpoints aligned at the centre of the plot. The result can be piped into \code{smoothHeatmap} and then \code{plotHeatmapList} or \code{plotHeatmapMeta}
 #'
 #' @param gr_ob GRanges object containing pairwise alignment
-#' @param gen_seq This should be either a BSgenome object or a DNAStringSet object, such that the genome sequence is contained in this input
+#' @param gen_seq (deprecated) This should be either a BSgenome object or a DNAStringSet object, such that the genome sequence is contained in this input.  Current version detects genome sequence automatically.
 #' @param basep_range range over which heatmap characteristic is plotted. Breakpoints will be aligned at the center of this.
 #' @param pat character string of desired pattern/characteristic to be plotted on heatmap
 #' @param ... Pass other arguments to \code{get_bps}.
@@ -15,7 +15,7 @@
 #' @importFrom heatmaps PatternHeatmap
 #' @importFrom Biostrings getSeq
 
-bp_heatmap <- function(gr_ob, gen_seq, basep_range, pat, ...){
+bp_heatmap <- function (gr_ob, gen_seq = NULL, basep_range, pat, ...) {
 
   # Suppress warnings about overflow
   suppressWarnings(
@@ -23,6 +23,15 @@ bp_heatmap <- function(gr_ob, gen_seq, basep_range, pat, ...){
   )
   # Remove the out-of-bound ranges.
   gr_bps <- gr_bps[gr_bps == trim(gr_bps)]
+
+  # Detect genome name and get BSgenome object
+  if (is.null(gen_seq)) {
+    gen_seq <- unique(genome(gr_ob))
+    if (length(gen_seq) != 1)
+      stop("Expected one genome name, found ", dQuote(gen_seq), " instead.")
+    # Convert from genome name to genome object
+    gen_seq <- get(gen_seq)
+  }
 
   # Heatmap
   hm_prep <- getSeq(gen_seq, gr_bps)
