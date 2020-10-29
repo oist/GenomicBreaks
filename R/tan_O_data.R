cc3abc8bac0c14ed0c5bdb106aec3aa4c03d2d8c
Charles Plessy <charles.plessy@oist.jp>
2020-10-29 05:02:06 +0000

    Complete the removal of old datasets.

--- R/tan_O_data.R
+++
@@ -1,7 +0,0 @@
-#' Tandem Repeat coverage of the Osaka2016 Genome
-#'
-#' Per-base tandem repeat coverage of the Osaka2016 genome. Tantan was used to determine the coverage.
-#'
-#' @format GRanges object
-#'
-"tan_O"
