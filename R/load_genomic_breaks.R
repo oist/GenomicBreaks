2264f439a0b2f35682a3fae8cc18530951c3d908
Charles Plessy <charles.plessy@oist.jp>
2020-12-07 10:56:49 +0900

    Refactor load_genomic_breaks().
     - Reflect the change of vocabulary, where "reference" genome is now
       to be called "target" genome.
    
     - Add a new "type" option to load pre-coalesced alignments
    produced by
       the maf-convert command.

--- R/load_genomic_breaks.R
+++ R/load_genomic_breaks.R
@@ -1,42 +1,60 @@
 #' Load pairwise genome alignments
 #'
-#' Loads alignments of a query genome to a reference genome from a text file in
-#' GFF3 format
+#' Loads alignments of a _query_ genome to a _target_ genome from a text file in
+#' GFF3 format.  By convention, the _target_ genome is the one that was indexed
+#' by the aligner.
 #'
 #' This function expects the pairwise alignment to be represented in GFF3 format
 #' in the following way:
 #'
-#' * Alignments blocks are represented by `match_part` entries in the _type_
-#'   column  Other entries will be discarded.
-#' * The coordinate system of the file is the one of the _reference_ genome.
+#' * Alignments blocks are represented by entries in specific sequence ontology
+#'   term in the _type_ column.  Other entries will be discarded.  The default
+#'   type is `match_part`.
+#' * The coordinate system of the file is the one of the _target_ genome.
 #' * The `Target` tag in the _attribute_ column contains the coordinates of
-#'   the match in the _query_ genome.
+#'   the match in the _query_ genome.  (Sorry that it is confusing…)
 #' * Stand information is set so that _query_ genome coordinates are always on
 #'   the _plus_ strand.
 #'
-#' @param file Path to a file in GFF3 format
-#' @param bsgenome_ref A `BSgenome` object representing the reference genome.
-#' @param bsgenome_query A `BSgenome` object representing the query genome.
+#' @param file Path to a file in GFF3 format.
+#' @param target_bsgenome A `BSgenome` object representing the target genome.
+#' @param query_bsgenome A `BSgenome` object representing the query genome.
 #' @param sort Returns the object sorted, ignoring strand information.
+#' @param type Sequence ontology term representing an alignment block
+#'        (default: `match_part`).
 #'
 #' @return Returns a `GRanges object` where each element represents a pairwise
 #' alignment block.  The `granges` part of the object contains the coordinates
-#' on the _reference_ genome, and the `query` metadata column contains the query
+#' on the _target_ genome, and the `query` metadata column contains the _query_
 #' coordinates in `GRanges` format.  The `seqinfo` of each `BSgenome` object
 #' passed as parameters are copied to the `GRanges` objects accordingly.
+#'
+#' @examples
+#' \dontrun{
+#' load_genomic_breaks(
+#'   system.file("extdata/OSKA2016__I69-5.gff3.gz", package="GenomicBreaks"),
+#'   target = OSKA2016,
+#'   query = OKI2018_I69)
+#' }
 #'
 #' @importFrom rtracklayer import.gff3
 #' @export
 
-load_genomic_breaks <- function(file, bsgenome_ref, bsgenome_query, sort = TRUE) {
+load_genomic_breaks <- function (
+  file,
+  target_bsgenome,
+  query_bsgenome,
+  sort = TRUE,
+  type = "match_part")
+{
   gb <- import.gff3(file)
-  gb <- GRanges(gb, seqinfo = seqinfo(bsgenome_ref))
+  gb <- GRanges(gb, seqinfo = seqinfo(target_bsgenome))
   # Discard cross_genome_match parent (used for block display in Zenbu)
-  gb <- gb[gb$type == "match_part"]
+  gb <- gb[gb$type == type]
   # Discard unused information
   gb$phase <- gb$Parent <- gb$Target <- gb$ID <- gb$source <- gb$type <- NULL
   # Convert query coordinates to GRanges
-  gb$query <- GRanges(gb$Name, seqinfo = seqinfo(bsgenome_query))
+  gb$query <- GRanges(gb$Name, seqinfo = seqinfo(query_bsgenome))
   gb$Name <- NULL
   if (sort) gb <- sort(gb, ignore.strand = TRUE)
   gb
