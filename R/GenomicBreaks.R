da9cc22b3038547076c079d962a18103de7334b0
Charles Plessy <charles.plessy@oist.jp>
2021-06-21 23:35:03 +0000

    Convert CNEr::Axt object to GBreaks object

--- R/GenomicBreaks.R
+++ R/GenomicBreaks.R
@@ -5,3 +5,23 @@
 #' @export
 
 GBreaks <- setClass("GBreaks", contains = "GRanges")
+
+
+#' Conversion from [`CNEr::Axt`] objects
+#'
+#' @importFrom CNEr first second
+#' @name as
+#' @export
+
+setAs("Axt", "GBreaks", function(from) {
+
+  # First genome of axt object is target genome, second is query.
+  gb       <- granges(first(from))
+  gb$query <- granges(second(from))
+
+  # In GBreaks object, strand information is carried by the target genome ranges
+  strand(gb) <- strand(gb$query)
+  strand(gb$query) <- "+"
+
+  GBreaks(gb)
+})
