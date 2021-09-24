28ef5522cca26441b348a79b3d69a31c85765003
Charles Plessy <charles.plessy@oist.jp>
2021-09-24 05:28:46 +0000

    Reform examples and rename variable names to reflect classes.

--- R/flagInversions.R
+++ R/flagInversions.R
@@ -6,52 +6,65 @@
 #' Here is a trivial example of an inversion.
 #'
 #' ```
-#' ┌───────────────┬───────────────┬───────────────┐
-#' │ XSR:101-200:+ │ XSR:201-300:- │ XSR:301-400:+ │  (OKI2018_I69)
-#' └───────────────┴───────────────┴───────────────┘
-#' ┌───────────────┬───────────────┬───────────────┐
-#' │  S1:101-200:+ │  S1:201-300:+ │  S1:301-400:+ │  (OSKA2016)
-#' └───────────────┴───────────────┴───────────────┘
+#' ┌──────────────┬──────────────┬──────────────┐
+#' │ chrA:101-200 │ chrA:201-300 │ chrA:301-400 │ (Target genome)
+#' └──────────────┴──────────────┴──────────────┘
+#'        +               -             +         (Alignment direction)
+#' ┌──────────────┬──────────────┬──────────────┐
+#' │ chrB:101-200 │ chrB:201-300 │ chrB:301-400 │ (Query genome)
+#' └──────────────┴──────────────┴──────────────┘
 #' ```
 #'
-#' @param gr_ob A `GenomicBreaks` object.
+#' @param gb A `GBreaks()` object.
 #' @param tol Tolerance window for the distance between two ranges.
 #'
-#' @return Returns the `GenomicBreaks` object with an extra `inv` metadata
-#' column.
+#' @return Returns the `GBreaks` object with an extra `inv` metadata column.
 #'
 #' @family Flagging functions
 #' @family Inversion functions
+#' @family Structural variants
 #'
 #' @examples
-#' inv       <- GRanges(c("XSR:101-200:+", "XSR:201-300:-",  "XSR:301-400:+"))
-#' inv$query <- GRanges(c( "S1:101-200",    "S1:201-300",     "S1:301-400"))
-#' flagInversions(inv)
+#' flagInversions(exampleInversion)
+#' plotApairOfChrs(exampleInversion)
+#'
+#' # Not an inversion
+#' flagInversions(exampleTranslocation)
+#' inv3       <- GRanges(c("XSR:101-180:-", "chr1:201-300:+",  "XSR:320-400:-"))
+#' inv3$query <- GRanges(c( "S1:101-200",    "S1:201-300",     "S1:301-400"))
+#' any(flagInversions(inv3)$inv) == FALSE
+#'
+#' # Also not an inversion
+#' inv4       <- GRanges(c("XSR:101-180:-", "XSR:1201-1300:+", "XSR:320-400:-"))
+#' inv4$query <- GRanges(c( "S1:101-200",    "S1:201-300",     "S1:301-400"))
+#' flagInversions(inv4)
+#' any(flagInversions(inv4)$inv) == FALSE
+#'
 #'
 #' @include dist2next.R
 #' @importFrom GenomicRanges precede
 #' @export
 
-flagInversions <- function (gr_ob, tol = Inf) {
-  gr <- gr_ob # save the original object
-  gr_ob <- dist2next(gr_ob, ignore.strand = TRUE)
-  gr_ob$tdist2 <- c(gr_ob$tdist[-1], Inf)
-  gr_ob$qdist2 <- c(gr_ob$qdist[-1], Inf)
-  gr_ob$oneNext <- precede(gr_ob$query, tail(gr_ob$query, -1), ignore.strand = T) - seq_along(gr_ob) == 0
-  gr_ob$twoNext <- c(gr_ob$oneNext[-1], NA)
-  gr_ob$oneDiffStrand <- strand(gr_ob) != c(strand(tail(gr_ob, -1)), factor("*"))
-  gr_ob$twoSameStrand <- strand(gr_ob) == c(strand(tail(gr_ob, -2)), factor(c("*", "*")))
-  gr$inv <- gr_ob$oneNext          &
-            gr_ob$twoNext          &
-            gr_ob$oneDiffStrand    &
-            gr_ob$twoSameStrand    &
-            gr_ob$tdist  < tol + 1 &   # We need `< tol + 1`
-            gr_ob$tdist2 < tol + 1 &   # and not `<= tol`
-            gr_ob$qdist  < tol + 1 &   # Because of comparisons to
-            gr_ob$qdist2 < tol + 1     # infinite distance returned by dist2next.
-  if (any(is.na(gr$inv)))
-    gr[is.na(gr$inv)]$inv <- FALSE
-  gr
+flagInversions <- function (gb, tol = Inf) {
+  gb.bak <- gb # save the original object
+  gb <- dist2next(gb, ignore.strand = TRUE)
+  gb$tdist2 <- c(gb$tdist[-1], Inf)
+  gb$qdist2 <- c(gb$qdist[-1], Inf)
+  gb$oneNext <- precede(gb$query, tail(gb$query, -1), ignore.strand = T) - seq_along(gb) == 0
+  gb$twoNext <- c(gb$oneNext[-1], NA)
+  gb$oneDiffStrand <- strand(gb) != c(strand(tail(gb, -1)), factor("*"))
+  gb$twoSameStrand <- strand(gb) == c(strand(tail(gb, -2)), factor(c("*", "*")))
+  gb.bak$inv <- gb$oneNext          &
+            gb$twoNext          &
+            gb$oneDiffStrand    &
+            gb$twoSameStrand    &
+            gb$tdist  < tol + 1 &   # We need `< tol + 1`
+            gb$tdist2 < tol + 1 &   # and not `<= tol`
+            gb$qdist  < tol + 1 &   # Because of comparisons to
+            gb$qdist2 < tol + 1     # infinite distance returned by dist2next.
+  if (any(is.na(gb.bak$inv)))
+    gb.bak[is.na(gb.bak$inv)]$inv <- FALSE
+  gb.bak
 }
 
 #' Show inversions and their flanking blocks.
@@ -114,12 +127,12 @@ filterInversions <- function(gb, rename = TRUE) {
 #' @examples
 #' inv       <- GRanges(c("XSR:101-180:+", "XSR:201-300:-",  "XSR:320-400:+"))
 #' inv$query <- GRanges(c( "S1:101-200",    "S1:201-300",     "S1:301-400"))
-#' inv <- flagInversions(inv)
+#' (inv <- flagInversions(inv))
 #' leftInversionGaps(inv)
 #'
 #' inv2       <- GRanges(c("XSR:101-180:-", "XSR:201-300:+",  "XSR:320-400:-"))
 #' inv2$query <- GRanges(c( "S1:101-200",    "S1:201-300",     "S1:301-400"))
-#' inv2 <- flagInversions(inv2)
+#' (inv2 <- flagInversions(inv2))
 #' leftInversionGaps(inv2)
 #'
 #' @export
