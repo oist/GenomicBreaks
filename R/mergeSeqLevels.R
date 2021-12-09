676d2b8c7f7e4090af47e2d56e2124d5a355bb19
Charles Plessy <charles.plessy@oist.jp>
2021-12-09 02:26:13 +0000

    Minor documentation edit.

--- R/mergeSeqLevels.R
+++ R/mergeSeqLevels.R
@@ -4,8 +4,8 @@
 #' sequences into larger ones.
 #'
 #' @note Be careful that in some cases it is needed to "flip" the sequence
-#' feature [`reverse`]' before merging, for instance when colinearity is with
-#' its reverse strand.
+#' feature with [`reverse`] before merging, for instance when colinearity is
+#' with its reverse strand.
 #'
 #' @param gr A `GRanges` object.
 #' @param seqs A character vector of [`seqlevels`] from `gr`
