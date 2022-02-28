a543a9a4fe22e9d5d3f25b446c658ddc34823430
Charles Plessy <charles.plessy@oist.jp>
2022-02-28 08:11:51 +0000

    New `sort` argument for `swap`.

--- NEWS.md
+++ NEWS.md
@@ -18,6 +18,8 @@
  * New `bridgeRegions()` function that maps unaligned regions of the _target_ to
    the _query_ genome when they are flanked by colinear regions.
  * `filterTranslocations()` gains a `remove` flag.
+ * `swap` gains a `sort` flag, so that in interactive sessions,
+   `gb |> swap() |> sort(i=T)` can be replaced by the shorter `gb |> swap(s = T)`.
 
 ## Backwards-incompatible changes
 
--- R/swap.R
+++ R/swap.R
@@ -1,9 +1,12 @@
 #' Swap target and query genomes
 #'
 #' Produce a new object in which the information about the _target_ and
-#' _query_ genomes have been inverted.
+#' _query_ genomes have been inverted.  As a large number of _GenomicBreaks_
+#' functions require `GBreaks` objects that were sorted with strand information
+#' ignored, a `sort` option is provided to do that.
 #'
-#' @param gb A [`GBreaks`] object
+#' @param gb A [`GBreaks`] object.
+#' @param sort Sort the returned object, ignoring strand.
 #'
 #' @return A new `GBreaks` object representing the input's _query_ ranges,
 #' with the input's _target_ ranges in the _query_ slot.  The strand information
@@ -12,14 +15,17 @@
 #' @family modifier functions
 #'
 #' @examples
-#' swap(exampleColinear3)
+#' swap(exampleDeletion)
+#' swap(exampleDeletion, sort = TRUE)
 #'
 #' @export
 
-swap <- function(gb) {
+swap <- function(gb, sort = FALSE) {
   gb2               <- gb$query
   gb2$query         <- granges(gb)
   strand(gb2)       <- strand(gb)
   strand(gb2$query) <- "*"
+  if (isTRUE(sort))
+    gb2 <- sort(gb2, ignore.strand = TRUE)
   GBreaks(gb2)
 }
