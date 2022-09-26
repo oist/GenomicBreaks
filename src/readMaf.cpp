dcca604747fe8d162d65166b4090ded7aa215991
Charles Plessy <charles.plessy@oist.jp>
2022-09-26 07:30:20 +0000

    Open the input file.  Dummy output

--- src/readMaf.cpp
+++ src/readMaf.cpp
@@ -1,11 +1,37 @@
 #include <Rcpp.h>
+
+#include <string.h>
+
+#include <algorithm>
+#include <cctype>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <stdexcept>
+#include <streambuf>
+
 using namespace Rcpp;
 //' Read a MAF file
 //'
-//' @param file The name of the file to read
+//' @param inputFileName The name of the file to read
 //' @return a GenomicBreaks object
 //' @export
+
+
+static void err(const std::string& s) {
+  throw std::runtime_error(s);
+}
+
+static std::istream& openIn(const std::string& fileName, std::ifstream& ifs) {
+  if (fileName == "-") return std::cin;
+  ifs.open(fileName.c_str());
+  if (!ifs) err("can't open file: " + fileName);
+  return ifs;
+}
+
 // [[Rcpp::export]]
-Rcpp::NumericVector readMAF (int A=10) {
-  return A;
+Rcpp::NumericVector readMAF (std::string inputFileName) {
+  std::ifstream inFileStream;
+  std::istream& input = openIn(inputFileName, inFileStream);
+  return 1;
 }
