422296734febb79638600e42bad6d2540e7601fa
Charles Plessy <charles.plessy@oist.jp>
2022-09-26 07:16:50 +0000

    Dummy readMAF function using Rcpp

---
+++ src/readMaf.cpp
@@ -0,0 +1,11 @@
+#include <Rcpp.h>
+using namespace Rcpp;
+//' Read a MAF file
+//'
+//' @param file The name of the file to read
+//' @return a GenomicBreaks object
+//' @export
+// [[Rcpp::export]]
+Rcpp::NumericVector readMAF (int A=10) {
+  return A;
+}
