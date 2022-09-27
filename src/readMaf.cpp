83b3c4f4bed9e71c6886bc7cec86bc8ff5d5854b
Charles Plessy <charles.plessy@oist.jp>
2022-09-27 08:17:58 +0000

    Return a list with seqnames, lengths, starts, etc.

--- src/readMaf.cpp
+++ src/readMaf.cpp
@@ -30,9 +30,16 @@ static std::istream& openIn(const std::string& fileName, std::ifstream& ifs) {
 }
 
 // [[Rcpp::export]]
-Rcpp::CharacterVector readMAF (std::string inputFileName) {
+Rcpp::List readMAF (std::string inputFileName) {
   Rcpp::CharacterVector seqnames1;
   Rcpp::CharacterVector seqnames2;
+  Rcpp::IntegerVector seqlengths1;
+  Rcpp::IntegerVector seqlengths2;
+  Rcpp::CharacterVector strands;
+  Rcpp::IntegerVector start1;
+  Rcpp::IntegerVector start2;
+  Rcpp::IntegerVector length1;
+  Rcpp::IntegerVector length2;
   std::string linetype;
   std::string seqname;
   int startpos;
@@ -53,14 +60,30 @@ Rcpp::CharacterVector readMAF (std::string inputFileName) {
       score = 1;
     }
     if (line[0] == 's') {
-      // Assume that there are only two 's' lines in a row
       // Example line: s BK006934.2   19127 1679 +  562643 AACCAATCCAAAA...
       std::stringstream(line) >> linetype >> seqname >> startpos >> length >> strand >> seqlength >> seq;
       seqnames1.push_back(seqname);
+      seqlengths1.push_back(seqlength);
+      start1.push_back(startpos);
+      length1.push_back(length);
+      // Assume that there are always two 's' lines in a row
       getline(inFileStream, line);
       std::stringstream(line) >> linetype >> seqname >> startpos >> length >> strand >> seqlength >> seq;
       seqnames2.push_back(seqname);
+      seqlengths2.push_back(seqlength);
+      start2.push_back(startpos);
+      length2.push_back(length);
+      strands.push_back(strand); // Assume that the strand of seq1 is always '+'
     }
   }
-  return seqnames1;
+  Rcpp::List outputList = Rcpp::List::create(Named("seqnames1") = seqnames1,
+                                             Named("seqlengths1") = seqlengths1,
+                                             Named("start1") = start1,
+                                             Named("length1") = length1,
+                                             Named("seqnames2") = seqnames2,
+                                             Named("seqlengths2") = seqlengths2,
+                                             Named("start2") = start2,
+                                             Named("length2") = length2,
+                                             Named("strand") = strands);
+  return outputList;
 }
