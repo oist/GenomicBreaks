2cf1e17d6fc54a4dbc27b72bdc45605d76b2d7c9
Charles Plessy <charles.plessy@oist.jp>
2022-09-27 07:11:47 +0000

    Read seqnames and more.

--- src/readMaf.cpp
+++ src/readMaf.cpp
@@ -32,14 +32,34 @@ static std::istream& openIn(const std::string& fileName, std::ifstream& ifs) {
 // [[Rcpp::export]]
 Rcpp::CharacterVector readMAF (std::string inputFileName) {
   Rcpp::CharacterVector seqnames1;
-  std::string seqname1;
+  Rcpp::CharacterVector seqnames2;
+  std::string linetype;
+  std::string seqname;
+  int startpos;
+  int length;
+  std::string strand;
+  int seqlength;
+  std::string seq;
   std::ifstream inFileStream;
   std::istream& input = openIn(inputFileName, inFileStream);
   std::string line;
+  int score;
   while(getline(inFileStream, line)) {
     if (line[0] != '#') {
-      std::stringstream(line) >> seqname1;
-      seqnames1.push_back(seqname1);
+      // do nothing
+    }
+    if (line[0] == 'a') {
+      // dummy score for the moment
+      score = 1;
+    }
+    if (line[0] == 's') {
+      // Assume that there are only two 's' lines in a row
+      // Example line: s BK006934.2   19127 1679 +  562643 AACCAATCCAAAA...
+      std::stringstream(line) >> linetype >> seqname >> startpos >> length >> strand >> seqlength >> seq;
+      seqnames1.push_back(seqname);
+      getline(inFileStream, line);
+      std::stringstream(line) >> linetype >> seqname >> startpos >> length >> strand >> seqlength >> seq;
+      seqnames2.push_back(seqname);
     }
   }
   return seqnames1;
