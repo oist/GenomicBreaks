c883402975d3116ec1a08db9c7ac33ececd774e7
Bruna Fernanda Fistarol_1 <b-fistarol@deigo010901.oist.jp>
2025-06-17 16:04:20 +0900

    Update hurdles function and simplifying superhurdles function

--- R/inversionDistance.R
+++ R/inversionDistance.R
@@ -10,8 +10,8 @@
 
 # Defining all required functions for the inversion distance
 
-#install.packages("igraph")
-#library(igraph)
+install.packages("igraph")
+library(igraph)
 
 
 
@@ -28,16 +28,13 @@ extendedPermutation <- function(p){
 }
 
 
-
-
-
 ##### Breakpoint Graph #####
 
 breakpoint_graph <- function(p_extended) {
   len_pe <- length(p_extended)
   len_p <- len_pe / 2 - 1
 
-  # initialize edge lists
+  # Initialize edge lists
   edge_list <- c()
   edge_colors <- c()
   edge_unoriented <- c()
@@ -46,14 +43,14 @@ breakpoint_graph <- function(p_extended) {
     a <- p_extended[2 * i + 1]
     b <- p_extended[2 * i + 2]
 
-    # black edge if not consecutive in genome permutation
+    # Black edge if not consecutive in genome permutation
     if (abs(a - b) != 1) {
       edge_list <- c(edge_list, a, b)
       edge_colors <- c(edge_colors, "black")
       edge_unoriented <- c(edge_unoriented, 1)
     }
 
-    # gray edge if not adjacent in the list
+    # Gray edge if not adjacent in the list
     i_init <- match(2 * i + 1, p_extended)
     i_end  <- match(2 * i + 2, p_extended)
 
@@ -64,7 +61,7 @@ breakpoint_graph <- function(p_extended) {
     }
   }
 
-  # build graph and assign attributes
+  # Build graph and assign attributes
   g <- make_empty_graph(n = len_pe, directed = FALSE)
   g <- add_edges(g, edge_list)
   E(g)$color <- edge_colors
@@ -78,14 +75,15 @@ breakpoint_graph <- function(p_extended) {
 # Check if two edges interleaves
 
 is_interleaving <- function(p_extended, graph_1, graph_2, edge_1, edge_2) {
-  # get positions of the two ends of each edge in the extended permutation
+
+  # Get positions of the two ends of each edge in the extended permutation
   e1_nodes <- ends(graph_1, edge_1)
   e2_nodes <- ends(graph_2, edge_2)
 
   e1_pos <- sort(match(e1_nodes, p_extended))
   e2_pos <- sort(match(e2_nodes, p_extended))
 
-  # check for interleaving: overlap without containment
+  # Check for interleaving: overlap without containment
   return((e1_pos[1] < e2_pos[1] && e2_pos[1] < e1_pos[2] && e1_pos[2] < e2_pos[2]) ||
            (e2_pos[1] < e1_pos[1] && e1_pos[1] < e2_pos[2] && e2_pos[2] < e1_pos[2]))
 }
@@ -117,7 +115,7 @@ components_graph <- function(g, p_extended) {
   comps <- components(g)
   comp_ids <- which(comps$csize > 1)
 
-  # map components to vertices
+  # Map components to vertices
   component_vertices <- lapply(comp_ids, function(i) which(comps$membership == i))
   component_subgraphs <- lapply(component_vertices, function(vids) induced_subgraph(g, vids))
 
@@ -126,7 +124,7 @@ components_graph <- function(g, p_extended) {
   min_extents <- sapply(component_vertices, function(verts) min(match(verts, p_extended)))
   max_extents <- sapply(component_vertices, function(verts) max(match(verts, p_extended)))
 
-  # build h with attributes
+  # Build h with attributes
   h <- make_empty_graph(n = length(comp_ids), directed = FALSE)
   V(h)$cycle_index <- comp_ids
   V(h)$unoriented <- unoriented_flags
@@ -138,14 +136,14 @@ components_graph <- function(g, p_extended) {
     for (i in 1:(length(h) - 1)) {
       vertices_i <- component_vertices[[i]]
       cycle_i <- component_subgraphs[[i]]
-      V(cycle_i)$name <- vertices_i  # set names for is_interleaving
+      V(cycle_i)$name <- vertices_i  # Set names for is_interleaving
 
       gray_i <- E(cycle_i)[E(cycle_i)$color == "gray"]
 
       for (j in (i + 1):length(h)) {
         vertices_j <- component_vertices[[j]]
         cycle_j <- component_subgraphs[[j]]
-        V(cycle_j)$name <- vertices_j  # same
+        V(cycle_j)$name <- vertices_j  # Same
 
         gray_j <- E(cycle_j)[E(cycle_j)$color == "gray"]
 
@@ -174,129 +172,113 @@ components_graph <- function(g, p_extended) {
 
 ##### Identifying hurdles #####
 
-hurdles_count <- function(g, p_extended) {
-  h <- components_graph(g, p_extended)
+hurdles_count <- function(g, query_sequence_unsig){
 
-  comps_h <- components(h)
-  comps_g <- components(g)
-  n_comps_h <- max(comps_h$membership)
+  h <- components_graph(g, query_sequence_unsig)
+  column_names <- c("membership", "min_extent", "max_extent", "contained",
+                    "contains", "hurdle", "superhurdle")
 
-  # Identify all unoriented components in h
-  is_unoriented <- sapply(1:n_comps_h, function(i) {
-    all(V(h)$unoriented[comps_h$membership == i])
-  })
-  unoriented_ids <- which(is_unoriented)
+  info <- data.frame(matrix(ncol = length(column_names), nrow = 0))
+  colnames(info) <- column_names
 
-  n_unoriented <- length(unoriented_ids)
-  if (n_unoriented == 0) {
-    return(data.frame(
-      membership = integer(0), min_extent = numeric(0), max_extent = numeric(0),
-      contained = integer(0), contains = integer(0),
-      hurdle = integer(0), superhurdle = integer(0)
-    ))
+  comps <- components(h)
+
+  # calculate the extent for each unoriented component of h
+  line <- 0
+  for (i in 1:max(comps$membership)) {
+    comp_vertices <- which(comps$membership == i)
+    if (all(V(h)$unoriented[comp_vertices])) {
+      line <- line + 1
+      info[line, "membership"] <- i
+      info[line, c("min_extent", "max_extent")] <- c(min(V(h)[comp_vertices]$min_extent),
+                                                     max(V(h)[comp_vertices]$max_extent))
+      info[line, c("contained", "contains", "hurdle", "superhurdle")] <- c(0, 0, 0, 0)
+    }
   }
 
-  # Preallocate info table
-  info <- data.frame(
-    membership = unoriented_ids,
-    min_extent = numeric(n_unoriented),
-    max_extent = numeric(n_unoriented),
-    contained = integer(n_unoriented),
-    contains = integer(n_unoriented),
-    hurdle = integer(n_unoriented),
-    superhurdle = integer(n_unoriented)
-  )
-
-  # Fill min and max extents
-  for (i in seq_along(unoriented_ids)) {
-    verts <- which(comps_h$membership == unoriented_ids[i])
-    info$min_extent[i] <- min(V(h)$min_extent[verts])
-    info$max_extent[i] <- max(V(h)$max_extent[verts])
-  }
-
-  if (n_unoriented == 1) {
-    info$hurdle[1] <- 1
+  if (line == 0) return(info)
+  if (line == 1) {
+    info[1, "hurdle"] <- 1
     return(info)
   }
 
-  # Determine containment relationships
-  contains_matrix <- outer(info$min_extent, info$min_extent, "<") &
-    outer(info$max_extent, info$max_extent, ">")
-  diag(contains_matrix) <- FALSE
+  # check pairwise containment between components
+  for (i in 1:(line - 1)) {
+    for (j in (i + 1):line) {
+      min_i <- info[i, "min_extent"]
+      max_i <- info[i, "max_extent"]
+      min_j <- info[j, "min_extent"]
+      max_j <- info[j, "max_extent"]
 
-  info$contains <- rowSums(contains_matrix)
-  info$contained <- colSums(contains_matrix)
-
-  # Determine hurdles
-  for (i in seq_len(n_unoriented)) {
-    if ((info$contained[i] == 0 || info$contained[i] == 1) && info$contains[i] == 0) {
-      info$hurdle[i] <- 1
+      if ((min_i < min_j) & (max_j < max_i)) {
+        info[i, "contains"] <- 1
+        info[j, "contained"] <- 1
+      }
+      if ((min_j < min_i) & (max_i < max_j)) {
+        info[j, "contains"] <- 1
+        info[i, "contained"] <- 1
+      }
     }
-    if (info$contained[i] == 0 && info$contains[i] == 1) {
-      info$hurdle[i] <- 2  # possible superhurdle
+  }
+
+  # determine which are hurdles (contain but not contained, or contained but not contain)
+  for (i in 1:line) {
+    if (info[i, "contains"] == 0) {
+      info[i, "hurdle"] <- 1
+    }
+    if (info[i, "contained"] == 0 && info[i, "contains"] == 1) {
+      info[i, "hurdle"] <- 2  # possible greatest hurdle; analyze further
     }
   }
 
   if (any(info$hurdle == 2)) {
-    # Check for actual superhurdle status
-    info_smallest <- which(info$hurdle == 1)
-    min_hurdles <- min(info$min_extent[info_smallest])
-    max_hurdles <- max(info$max_extent[info_smallest])
-
     possible_greatest <- which(info$hurdle == 2)
+    min_hurdles <- min(info[info$hurdle == 1, "min_extent"])
+    max_hurdles <- max(info[info$hurdle == 1, "max_extent"])
+
     for (i in possible_greatest) {
-      if (info$min_extent[i] > min_hurdles || info$max_extent[i] < max_hurdles) {
-        info$hurdle[i] <- 0
+      if (info[i, "min_extent"] > min_hurdles || info[i, "max_extent"] < max_hurdles) {
+        info[i, "hurdle"] <- 0
       }
     }
 
-    # If still a potential superhurdle, validate separation
-    super_candidate <- which(info$hurdle == 2)
-    if (length(super_candidate) == 1) {
-      sc_idx <- super_candidate[1]
-      info$hurdle[sc_idx] <- 1  # start assuming it's valid
+    if (any(info$hurdle == 2)) {
+      idx_row <- which(info$hurdle == 2)
+      comp_id <- info[idx_row, "membership"]
+      comp_vertices <- which(comps$membership == comp_id)
+      cycle_indices <- V(h)$cycle_index[comp_vertices]
+      info_smallest <- which(info$hurdle == 1)
 
-      comp_id <- info$membership[sc_idx]
-      comp_vertices <- which(comps_h$membership == comp_id)
-      cycle_ids <- V(h)$cycle_index[comp_vertices]
+      # assume it's a hurdle unless proven otherwise
+      info[idx_row, "hurdle"] <- 1
+      if (length(info_smallest) == 1) return(info)
 
-      if (length(info_smallest) > 1) {
-        edge_colors <- E(g)$color == "gray"
-        gray_edges <- which(edge_colors)
-        edge_ends <- ends(g, gray_edges, names = FALSE)
-        positions <- match(edge_ends, p_extended)
+      for (cycle in cycle_indices) {
+        gray_edges <- E(g)[components(g)$membership == cycle & E(g)$color == "gray"]
+        for (j in 1:(length(info_smallest) - 1)) {
+          for (k in (j + 1):length(info_smallest)) {
+            min_j <- info[info_smallest[j], "min_extent"]
+            max_j <- info[info_smallest[j], "max_extent"]
+            min_k <- info[info_smallest[k], "min_extent"]
+            max_k <- info[info_smallest[k], "max_extent"]
 
-        for (i in cycle_ids) {
-          edge_indices <- which(comps_g$membership[gray_edges] == i)
-          if (length(edge_indices) == 0) next
+            for (edge in gray_edges) {
+              edge_extent <- sort(match(ends(g, edge), query_sequence_unsig))
+              condition_1 <- (edge_extent[1] < min_j && edge_extent[2] > max_j &&
+                                (edge_extent[1] > max_k || edge_extent[2] < min_k))
+              condition_2 <- (edge_extent[1] < min_k && edge_extent[2] > max_k &&
+                                (edge_extent[1] > max_j || edge_extent[2] < min_j))
 
-          for (j in 1:(length(info_smallest)-1)) {
-            for (k in (j+1):length(info_smallest)) {
-              min_j <- info$min_extent[info_smallest[j]]
-              max_j <- info$max_extent[info_smallest[j]]
-              min_k <- info$min_extent[info_smallest[k]]
-              max_k <- info$max_extent[info_smallest[k]]
-
-              for (e_idx in edge_indices) {
-                edge_pos <- sort(positions[e_idx, ])
-
-                condition_1 <- (edge_pos[1] < min_j && edge_pos[2] > max_j &&
-                                  (edge_pos[1] > max_k || edge_pos[2] < min_k))
-                condition_2 <- (edge_pos[1] < min_k && edge_pos[2] > max_k &&
-                                  (edge_pos[1] > max_j || edge_pos[2] < min_j))
-
-                if (condition_1 || condition_2) {
-                  info$hurdle[sc_idx] <- 0
-                  break
-                }
+              if (condition_1 || condition_2) {
+                info[idx_row, "hurdle"] <- 0
+                break
               }
-
-              if (info$hurdle[sc_idx] == 0) break
             }
-            if (info$hurdle[sc_idx] == 0) break
+            if (info[idx_row, "hurdle"] == 0) break
           }
-          if (info$hurdle[sc_idx] == 0) break
+          if (info[idx_row, "hurdle"] == 0) break
         }
+        if (info[idx_row, "hurdle"] == 0) break
       }
     }
   }
@@ -312,20 +294,16 @@ hurdles_count <- function(g, p_extended) {
 
 superhurdles_count <- function(info, g, query_sequence_unsig){
 
-  #if the number of hurdles is even, it is not necessary to calculate superhurdles because of the definition of fortress
-
+  # If the number of hurdles is even, it is not necessary to calculate superhurdles because of the definition of fortress
   if (sum(info$hurdle)%%2 == 0) {return(info)}
 
-  #if all unoriented component are hurdles, there is no hurdle protecting an unoriented nonhurdle
-
+  # If all unoriented component are hurdles, there is no hurdle protecting an unoriented nonhurdle
   if (sum(info$hurdle) == length(info$hurdle)) {return(info)}
 
-  #for all hurdles: remove one by one and check if some non hurdle turned into a hurdle; if it is the case, then the removed hurdle is a superhurdle
-
+  # For all hurdles: remove one by one and check if some non hurdle turned into a hurdle; if it is the case, then the removed hurdle is a superhurdle
   for (ignored in info$membership[info$hurdle == 1]){
 
-    # remove "ignored" to calculate hurdles
-
+    # Remove "ignored" to calculate hurdles
     info2 <- subset(info, membership != ignored)
 
     line <- length(info2$membership)
