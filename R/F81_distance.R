af1bc63773e0b6fd427a9405fe3482492325a023
Charles Plessy <charles.plessy@oist.jp>
2025-10-06 10:00:21 +0900

    Reflow

--- R/F81_distance.R
+++ R/F81_distance.R
@@ -17,11 +17,13 @@
 #' @author Charles Plessy
 #'
 #' @references
-#' Felsenstein, J. (1981). Evolutionary trees from DNA sequences: a maximumlikelihood approach.
-#' *Journal of Molecular Evolution*, 17, 368–376. \doi{10.1007/BF01734359}
+#' Felsenstein, J. (1981). Evolutionary trees from DNA sequences: a maximum
+#' likelihood approach.  *Journal of Molecular Evolution*, 17, 368–376.
+#' \doi{10.1007/BF01734359}
 #'
-#' McGuire, G., Prentice, M. J., & Wright, F. (1999). Improved error bounds for genetic distances from DNA sequences.
-#' *Biometrics*, 55(4), 1064–1070. \doi{10.1111/j.0006-341x.1999.01064.x}
+#' McGuire, G., Prentice, M. J., & Wright, F. (1999). Improved error bounds for
+#' genetic distances from DNA sequences.  *Biometrics*, 55(4), 1064–1070.
+#' \doi{10.1111/j.0006-341x.1999.01064.x}
 #'
 #' @examples
 #' parameters <- readTrainFile(system.file("extdata/example.train", package = "GenomicBreaks"))
