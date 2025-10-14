207afa25023066a092b835c0be690e533d83cde1
Charles Plessy <charles.plessy@oist.jp>
2025-10-14 13:52:03 +0900

    Streamline SRI documentation and cite paper.

--- R/strand_randomisation_index.R
+++ R/strand_randomisation_index.R
@@ -5,19 +5,19 @@
 #' on which homologous sequences align appears to be randomised.  This index
 #' expresses it with a number.
 #'
-#' For each feature of the _target_ genome, the total length of its alignments
-#' on the plus strand is subtracted from the total length of its alignments
-#' on the minus strand.  The sign of this number is removed because there is
-#' no guarantee that homologous chromosomes are sequenced in the same direction
-#' in different assemblies.  To make features comparable, this number is then
-#' normalised by total number of aligned bases.  Then, the computed numbers for
-#' all the _target_'s features are averaged with a weighted mean, where the
-#' weight is the total number of aligned bases of that feature.  Thus, a number
-#' close to 1 is expected for closely related genomes.
+#' For each feature in the _target_ genome, the total length of alignments on
+#' the plus strand is subtracted from that on the minus strand, and the absolute
+#' value is taken (because there is no guarantee that homologous chromosomes are
+#' sequenced in the same direction in different assemblies).  This value is
+#' normalized by the total number of aligned bases.  A weighted mean is then
+#' computed across all features, using each feature’s total aligned bases as its
+#' weight.  Thus, a number close to 1 is expected for closely related genomes.
 #'
 #' @note This index is designed for comparison of chromosomal assemblies that
 #' have a strong conservation of synteny in the sense most homologous genes
-#' are mapped on pairs of homologous chromosomes.
+#' are mapped on pairs of homologous chromosomes.  In 2025, a new `tiles` option
+#' was added to make the index more robust to assembly errors and karyotype
+#' changes.
 #'
 #' @param gb A [`GBreaks`] object.
 #' @param tiles A number of tiles
@@ -25,6 +25,15 @@
 #' @family Similarity indexes
 #'
 #' @returns Returns a numeric value between 0 and 1.
+#'
+#' @references Plessy C, Mansfield MJ, Bliznina A, Masunaga A, West C, Tan Y,
+#' Liu AW, Grašič J, Del Río Pisula MS, Sánchez-Serna G, Fabrega-Torrus M,
+#' Ferrández-Roldán A, Roncalli V, Navratilova P, Thompson EM, Onuma T, Nishida
+#' H, Cañestro C, Luscombe NM (2024). Extreme genome scrambling in marine
+#' planktonic _Oikopleura dioica_ cryptic species. *Genome Research*, 34(3),
+#' 426–440. \url{https://doi.org/10.1101/gr.278295.123}. PMID: 38621828
+#'
+#' @author Charles Plessy
 #'
 #' @examples
 #' strand_randomisation_index(exampleInversion)
