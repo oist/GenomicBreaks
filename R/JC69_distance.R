8530445412ea685159db26a2172dec30a2bdede0
pribiller <pribiller@gmail.com>
2026-08-19 15:39:29 +0900

    Make the JC69 function more generic Add a generic function to handle different types of input. Add an alternative JC69 function that takes as input only (1) the number of mismatches and (2) the total number of base pairs. This allows the user to choose how to define the total base pairs (e.g., using the whole sequence, a subset, only aligned regions, etc.).

--- R/JC69_distance.R
+++ R/JC69_distance.R
@@ -4,28 +4,58 @@
 #'
 #' @references Jukes, T.H. & Cantor, C.R. (1969). "Evolution of protein molecules." In *Mammalian Protein Metabolism* (pp. 21–132). Academic Press.
 #'
-#' @param m A matrix of counts or probabilities for bases of the _target_ genome
-#' to be aligned to bases on the _query_ genome.  As a convenience it can also
-#' receive a list produced by the [`readTrainFile()`] function, containing this
-#' matrix.
+#' @param val Three possible types of input are available:
+#' 1. A matrix of counts or probabilities for bases of the _target_ genome to be aligned to bases on the _query_ genome;
+#' 2. A list produced by the [`readTrainFile()`] function, containing the matrix described in (1);
+#' 3. Two numbers and an optional boolean flag:
+#'    * `mis` The mismatch count (a positive integer) or proportion (a number between 0.0 and 1.0).
+#'    * `tot` The total count of nucleotides, in case the mismatches are represented as a count.
+#'    * `adjust_p` A boolean flag. If TRUE, the distance is scaled between 0 and 0.75 to ensure the logarithm stays positive.
 #'
 #' @family Alignment statistics
 #' @family Similarity indexes
 #'
 #' @author Zikun Yang
+#' @author Priscila Biller
 #'
 #' @returns Returns a numeric value show the evolutionary distance between two genomes. the larger the value, the more different the two genomes are.
 #'
 #' @examples
-#' JC69_distance(exampleSubstitutionMatrix)
+#' # Using a substitution matrix as input:
+#' d <- JC69_distance(exampleSubstitutionMatrix)
+#'
+#' # Using numerical values as input:
+#' d <- JC69_distance(25, 100)
 #'
 #' @export
+JC69_distance <- function(val,...) {
+  UseMethod("JC69_distance")
+} 
 
-JC69_distance <- function(m) {
-  if(is.list(m)) m <- m$probability_matrix
+#' @export
+JC69_distance.numeric <- function(mis, tot=1.0, adjust_p=FALSE) {
+  # The p-distance, i.e., the fraction of nucleotides that are different.
+  p <- mis / tot
+  # In this model, at equilibrium, the p-distance between two sequences
+  # that began as identical should be 75%, and should never exceed 75%.
+  # This 'trick' ensures the log argument stays valid (and the expression
+  # remains well-behaved) even if the raw distance would be above 75%.
+  if (adjust_p == TRUE) { p <- p * 0.75 }
+  # Case where log is negative: the distance is undefined.
+  else if (p > 0.75) {return(NA)}
+  - 0.75 * log(1 - 4 * p / 3)
+}
+
+#' @export
+JC69_distance.matrix <- function(m) {
   m <- m[c("A", "C", "G", "T"), c("A", "C", "G", "T")]
   if (all(m == 0)) return(NA)
   P <- prop.table(m)
   p <- 1 - sum(diag(P))
-  - 0.75 * log(1 - 4 * p / 3)
+  JC69_distance(p)
+}
+
+#' @export
+JC69_distance.list <- function(l) {
+  JC69_distance(l$probability_matrix)
 }
